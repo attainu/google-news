4b09fe877f94ce8f17dd3f47440205935e2a2057
Akash Sharma <akash2909as@gmail.com>
2020-08-20 17:39:22 +0530

    add no data message in following-bookmark page

--- src/Component/Card/FollowingCardList.jsx
+++ src/Component/Card/FollowingCardList.jsx
@@ -3,7 +3,7 @@ import FollowingCard from "./FollowingCard";
 import './css/SourceCardList.css'
 
 const FollowingCardList = ({ stories }) => {
-    return (
+    return stories == '' ?<h1 style={{marginLeft:"300px", marginTop:"250px", fontSize:"5rem",color:'#576574'}}>No data found</h1>:(
         <div className="cardList">
             {console.log(stories)}
             {stories.map((article) => (
