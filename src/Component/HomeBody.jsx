53f5f931be3ab316850816daaac6887ca5fd68b9
Akash Sharma <akash2909as@gmail.com>
2020-08-21 03:25:43 +0530

    changes in tab

--- src/Component/HomeBody.jsx
+++ src/Component/HomeBody.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import WeatherCard from "./WeatherCard/WeatherCard";
 import "./css/HomeBody.css";
 import { Tabs } from "@feuer/react-tabs";
 import PostCardList from "./Card/PostCardList";
 import { connect } from "react-redux";
 import { fetchStories } from "../redux/action/storiesAction";
-import { fetchSource } from "../redux/action/sourceAction";
+import { fetchCategoryStories } from "../redux/action/categoryActions";
 import SourceCardList from "./Card/SourceCardList";
 import SwipeCard from "./SwipeCard/SwipeCard";
 
 class HomeBody extends Component {
   componentDidMount() {
     this.props.fetchStories("top-headlines", 1, 5);
-    this.props.fetchSource();
+    this.props.fetchCategoryStories("business", 1, 5);//have to pass 1st parameter dynamically according to tab title so that it will fetch data respective to tab title name
   }
+  handleTabClick = (e) => {};
   render() {
     const loaderStyle = {
       marginLeft: "600px",
@@ -40,14 +42,34 @@ class HomeBody extends Component {
             {this.props.isFetching ? (
               <div className="loader" style={loaderStyle}></div>
             ) : (
-              <PostCardList stories={this.props.stories} />
+              <>
+                <PostCardList stories={this.props.stories} />
+                <Link to="/topStories">
+                  <button style={{ marginLeft: "550px" }}>View More</button>
+                </Link>
+              </>
             )}
           </Tabs.Tab>
-          <Tabs.Tab id="source" title="Source">
-            {this.props.isFetching ? (
+
+          <Tabs.Tab id="business" title="Business">
+            {this.props.isCategoryFetching ? (
               <div className="loader" style={loaderStyle}></div>
             ) : (
-              <SourceCardList stories={this.props.source} />
+              <PostCardList stories={this.props.categoryStories} />
+            )}
+          </Tabs.Tab>
+          <Tabs.Tab id="technology" title="Technology">
+            {this.props.isCategoryFetching ? (
+              <div className="loader" style={loaderStyle}></div>
+            ) : (
+              <PostCardList stories={this.props.categoryStories} />
+            )}
+          </Tabs.Tab>
+          <Tabs.Tab id="entertainment" title="Entertainment">
+            {this.props.isCategoryFetching ? (
+              <div className="loader" style={loaderStyle}></div>
+            ) : (
+              <PostCardList stories={this.props.categoryStories} />
             )}
           </Tabs.Tab>
         </Tabs>
@@ -61,10 +83,12 @@ const mapStateToProps = (storeState) => {
     stories: storeState.storiesStatus.stories,
     isFetching: storeState.storiesStatus.isFetching,
 
-    source: storeState.sourceStatus.source,
+    categoryStories: storeState.categoryPageStatus.stories,
+    isCategoryFetching: storeState.categoryPageStatus.isFetching,
   };
 };
 
-export default connect(mapStateToProps, { fetchStories, fetchSource })(
+export default connect(mapStateToProps, { fetchStories, fetchCategoryStories })(
   HomeBody
 );
+
--- src/redux/action/categoryActions.js
+++ src/redux/action/categoryActions.js
@@ -4,12 +4,14 @@ import {
   TOGGLE_CATEGORY_FETCHING_STATE,
 } from "../actionTypes";
 
-export const fetchCategoryStories = (categoryName) => async (dispatch) => {
+export const fetchCategoryStories = (categoryName,pageNumber,pagesize) => async (dispatch) => {
   try {
     dispatch({ type: TOGGLE_CATEGORY_FETCHING_STATE });
 
     const response = await axios(
-      `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=in&category=${categoryName}&pageSize=100&apiKey=030c64300b5b4d14b11c5b91c2e25946`
+      `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=in&category=${categoryName}${
+        pagesize ? `&pageSize=+${pagesize}` : ""
+      }&page=${pageNumber}&apiKey=030c64300b5b4d14b11c5b91c2e25946`
     );
 
     const data = response.data.articles;
